d0777eb158a3f29f35da34865278d97fc1497d20
白 <934752907@qq.com>
2021-05-11 09:47:02 +0800

    提交

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'antd/dist/antd.min.css'
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default App
